9b288e1e7f03e62f31581ad6fe403288a76128cf
musesum <musesum@gmail.com>
2025-04-23 17:20:15 -0700

    Exported types

--- Sources/MuSky/Resources/cell.flo.h
+++ Sources/MuSky/Resources/cell.flo.h
@@ -1,37 +1,37 @@
 cell (columns 2, svg "icon.cellular.automata") {
 
     slide ('Slide Bit Planes',
-           seg, y 0_7~3,
+           seg, x 0_7~3,
            img "icon.cell.slide",
            <> pipe.cell˚slide.version)
     
     zha   ('Cellular Automata Belousov–Zhabotinsky spirals',
-           seg, y 0_6~2,
+           seg, x 0_6~2,
            img "icon.cell.zha",
            <> pipe.cell˚zha.version)
     
     ave   ('Cellular Automata Averaging nearby pixels',
-           val, y 0…1~0.5,
+           val, x 0…1~0.5,
            img "icon.cell.ave",
            <> pipe.cell˚ave.version)
     
     fade  ('Fade out to zero -- interesting in higher bit planes',
-           val, y 0.2…2~1,
+           val, x 0.2…2~1,
            img "icon.cell.fade",
            <> pipe.cell˚fade.version)
     
     melt  ('Cellular Automata Reaction Diffusion rule - flashy at first',
-           val, y 0…1~0.5,
+           val, x 0…1~0.5,
            img "icon.cell.melt",
            <> pipe.cell˚melt.version)
     
     tunl  ('Cellular Automata Tube Tunnel rule',
-           seg, y 0_5~1,
+           seg, x 0_5~1,
            img "icon.cell.tunl",
            <> pipe.cell˚tunl.version)
     
     fred  ('Cellular Automata Fredkin rule',
-           seg, y 0_4~4,
+           seg, x 0_4~4,
            img "icon.cell.fred",
            <> pipe.cell˚fred.version)
 }
