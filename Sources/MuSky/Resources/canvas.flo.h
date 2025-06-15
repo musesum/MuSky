66a63c09b629bb6bd16177e046d059c9d5c32fe4
musesum <musesum@gmail.com>
2025-06-15 00:31:46 -0700

    refactor MenuOp

--- Sources/MuSky/Resources/canvas.flo.h
+++ Sources/MuSky/Resources/canvas.flo.h
@@ -1,18 +1,18 @@
 canvas (columns 2,
         svg "icon.canvas") {
-    
+
     color ('shift color frame and palette fade',
            xy, x 0…1, y 0…1,
            sym "paintpalette",
            <> (pipe.color.plane,
                sky.color.xfade),
            ^- sky.main.anim)
-    
+
     brush ('brush size',
            val, x 1_255=255,
            sym "paintbrush.pointed",
            <> sky.draw.brush.index)
-    
+
     repeat ('repeat canvas, 0 is 1:1',
             xy, x -1…1=0, y -1…1=0,
             svg "icon.repeat.arrows",
@@ -20,28 +20,28 @@ canvas (columns 2,
             -> (midi.cc.skypad.repeatX(val: x),
                 midi.cc.skypad.repeatY(val: y)),
             ^- sky.main.anim)
-    
+
     mirror ('mirror repetitions',
             xy, x 0…1=0, y 0…1=0,
             svg "icon.mirror",
             <> pipe˚.mirror,
             ^- sky.main.anim)
-    
+
     size  ('fixed brush size',
            val, x 1_64=12 : 12,
            svg "icon.size.dot",
            <> sky.draw.brush.size)
-    
+
     press ('set on to let iPad pencil or AVP depth change size',
            tog, x 1,
            sym "scribble.variable",
            <> sky.draw.brush.press)
-    
+
     tilt  ('set on to let iPad tilt shift screen',
            tog, x 0,
            sym "angle",
            <> sky.input.tilt)
-    
+
     shift ('amount of screen shift, double tap to stop',
            xy,
            x 0…1=0.5, y 0…1=0.5,
@@ -51,7 +51,7 @@ canvas (columns 2,
 
     alpha (xy, x 0…1=1, y 0…1=1,
            sym "cube.transparent",
-           //.....-> pipe.render.map.cube.mixcube(y: x),
+           '-> pipe.render.map.cube.mixcube(y: x)',
            -> plato.alpha(y: y))
 
     fill  ('erase screen with fill color',
--- Sources/MuSky/Resources/more.flo.h
+++ Sources/MuSky/Resources/more.flo.h
@@ -1,33 +1,33 @@
 more (svg "icon.more") {
-    
+
     bonjour (peer "bonjour",
              sym "bonjour",
              <> sky.network.bonjour)
-    
+
     archive (arch,
              sym "building.columns")
-    
 
+    search('voice search',
+           sym "magnifyingglass",
+           <> sky.menu.search)
+    
     _fps (val, 0_60=60,
           img "icon.speed",
           <> sky.main.fps)
-    
+
     _anim(val, 0…1:0.24,
           sym "bolt.fill",
           <> sky.main.anim)
-    
+
     _snapshot('take a shapshot of current image',
               tog, x 0,
               sym "camera.shutter.button")
-    
+
     _follow (tog, x 1,
              sym "app.connected.to.app.below.fill",
              <> sky.network.follow)
-    
+
     _midi (tog, x 1,
            sym "pianokeys.inverse",
            <> sky.network.midi)
 }
-search('voice search',
-       sym "magnifyingglass",
-       <> sky.menu.search)
