08fe5d37cdbdd7fbfc08316029f4f13091ecf24d
musesum <musesum@gmail.com>
2025-06-25 08:26:16 -0700

    fix VisionOS26 runtime crash

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 
 let package = Package(
     name: "MuSky",
-    platforms: [.iOS(.v17)],
+    platforms: [.iOS(.v17),
+                .visionOS(.v2)],
     products: [.library(name: "MuSky", targets: ["MuSky"])],
     dependencies: [],
     targets: [
