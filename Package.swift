a57284f916bdc7a0fe417613b4ed8bf637ada8b6
musesum <musesum@gmail.com>
2025-06-26 16:25:34 -0700

    interim actor-isolated

--- Package.swift
+++ Package.swift
@@ -1,11 +1,10 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
     name: "MuSky",
-    platforms: [.iOS(.v17),
-                .visionOS(.v2)],
+    platforms: [.iOS(.v17), .visionOS(.v2)],
     products: [.library(name: "MuSky", targets: ["MuSky"])],
     dependencies: [],
     targets: [
