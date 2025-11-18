d6bb4be831e4d476f7ab2c9df99bccf4c6b1647d
musesum <musesum@gmail.com>
2025-11-18 05:23:18 -0800

    draw.flo

--- Sources/MuSky/Resources/brush.flo.h
+++ Sources/MuSky/Resources/brush.flo.h
@@ -4,26 +4,26 @@ brush ('brush',
     index ('index',
           val, x 1_255=255,
           sym "square.stack.3d.up",
-          <> sky.draw.brush.index)
+          <> draw.brush.index)
 
     size  ('fixed brush size',
            val, x 1_128=12 : 12,
            svg "icon.size.dot",
-           <> sky.draw.brush.size)
+           <> draw.brush.size)
 
     press ('set on to let iPad pencil or AVP depth change size',
            tog, x 1,
            sym "scribble.variable",
-           <> sky.draw.brush.press)
+           <> draw.brush.press)
 
     tilt  ('set on to let iPad tilt shift screen',
            tog, x 0,
            sym "angle",
-           <> sky.input.tilt)
+           <> input.tilt)
 
     fill  ('erase screen with fill color',
            val, x 0…1=0 : 0,
            sym "drop",
-           <> sky.draw.screen.fill)
+           <> draw.screen.fill)
 }
 
--- Sources/MuSky/Resources/sky.flo.h
+++ Sources/MuSky/Resources/sky.flo.h
@@ -23,36 +23,4 @@ sky ('visual music synth') {
         tilt    (x 0…1=1,  'for iPad pen, allow tilt to shift screen')
         force   (x 0…0.5, -> draw.brush.size, 'iPad pen, pressure with change brush size')
     }
-    draw ('draw on metal layer') {
-        screen.fill(x 0…1=0, 'fill cellular automata universe')
-        brush ('type of brush and range') {
-            size  (x 1…128=10:10, 'range of radius')
-            press (x 0…1=1, 'pressure changes size')
-            index (x 1…255=127, 'index in 256 color palette')
-        }
-        line ('place holder for line drawing') {
-            prev (x 0…1, y 0…1,'staring point of segment')
-            next (x 0…1, y 0…1,'endint point of segment')
-        }
-        dot(on 1, log 0, 'use MIDI MPI input to drawing dots on canvas') {
-            note {
-                on(chan, num, velo, <- midi.input.note.on)
-                off(chan, num, velo, <- midi.input.note.off)
-            }
-            after(chan, val, <- midi.input.afterTouch)
-            wheel(chan, val, <- midi.input.pitchWheel)
-            slide(chan, cc == 74, val, <- midi.input.controller)
-            clear(<- draw.screen.fill)
-        }
-        ripple(on 1, log 0, 'use MIDI MPE to ripple colors through pal0/pal1 ') {
-            note {
-                on(chan, num, velo, <- midi.input.note.on)
-                off(chan, num, velo, <- midi.input.note.off)
-            }
-            after(chan, val, <- midi.input.afterTouch)
-            wheel(chan, val, <- midi.input.pitchWheel)
-            slide(chan, cc == 74, val, <- midi.input.controller)
-            clear(<- draw.screen.fill)
-        }
-    }
 }
