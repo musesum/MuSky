74fd0a1c9f7cf0f9e9c8bc3bd9dd4ad58c404548
musesum <musesum@gmail.com>
2025-11-18 15:10:11 -0800

    tape tog

--- Sources/MuSky/Resources/tape.flo.h
+++ Sources/MuSky/Resources/tape.flo.h
@@ -1,9 +1,9 @@
 tape (columns 3, 'record playback loop', sym "recordingtape") {
-    rec  ('record', sym "record.circle.fill", on 0)
-    play ('playback', sym "play.fill", on 0)
-    stop ('stop', sym "stop.fill", on 0)
-    forw ('forward', sym "forward.frame.fill", on 0)
-    loop ('looping', sym "point.forward.to.point.capsulepath", on 0)
-    learn ('learn', sym "graduationcap", on 0)
-    *(-> *(on 0)) // solo only one
+    rec  ('record'  , tog, x 0, sym "record.circle.fill")
+    play ('playback', tog, x 0, sym "play.fill")
+    stop ('stop'    , tog, x 0, sym "stop.fill")
+    forw ('forward' , tog, x 0, sym "forward.frame.fill")
+    loop ('looping' , tog, x 0, sym "point.forward.to.point.capsulepath")
+    learn ('learn'  , tog, x 0, sym "graduationcap")
+    *(-> *(x 0)) // solo only one
 }
