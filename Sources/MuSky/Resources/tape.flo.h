7f53ce65af64fd83ad5a99b79929274b085e59e2
musesum <musesum@gmail.com>
2025-11-20 15:17:05 -0800

    refactor Tape, Mirror.  Add LeafTap.

--- Sources/MuSky/Resources/tape.flo.h
+++ Sources/MuSky/Resources/tape.flo.h
@@ -1,9 +1,8 @@
-tape (columns 3, 'record playback loop', sym "recordingtape") {
-    rec  ('record'  , tog, x 0, sym "record.circle.fill")
-    play ('playback', tog, x 0, sym "play.fill")
-    stop ('stop'    , tog, x 0, sym "stop.fill")
-    forw ('forward' , tog, x 0, sym "forward.frame.fill")
-    loop ('looping' , tog, x 0, sym "point.forward.to.point.capsulepath")
-    learn ('learn'  , tog, x 0, sym "graduationcap")
+tape ('record playback loop', sym "recordingtape") {
+    record (tog, x 0, sym "record.circle.fill")
+    play   (tog, x 0, sym "play.fill")
+    loop   (tog, x 0, sym "point.forward.to.point.capsulepath")
+    learn  (tog, x 0, sym "graduationcap")
+    beat   (tap, x 0, svg "icon.heartbeat")
     *(-> *(x 0)) // solo only one
 }
