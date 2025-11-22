fdf3c54692f0eef422163ede20600a77fa37e005
musesum <musesum@gmail.com>
2025-11-22 13:26:17 -0800

    Icon On Off

--- Sources/MuSky/Resources/tape.flo.h
+++ Sources/MuSky/Resources/tape.flo.h
@@ -1,8 +1,22 @@
 tape ('record playback loop', sym "recordingtape") {
-    record (tog, x 0, sym "record.circle.fill")
-    play   (tog, x 0, sym "play.fill")
-    loop   (tog, x 0, sym "point.forward.to.point.capsulepath")
-    learn  (tog, x 0, sym "graduationcap")
-    beat   (tap, x 0, svg "icon.heartbeat")
-    *(-> *(x 0)) // solo only one
+    record (tog, x 0, -> play(x 0)) {
+        _on  (sym "record.circle.fill")
+        _off (sym "square.fill")
+    }
+    play (tog, x 0, -> record(x 0)) {
+        _on  (sym "play.fill")
+        _off (sym "play")
+    }
+    loop (tog, x 0, -> record(x 0)) {
+        _on  (sym "arrow.trianglehead.clockwise")
+        _off (sym "arrow.forward.to.line.compact")
+    }
+    learn (tog, x 0) {
+        _on  (sym "graduationcap.fill")
+        _off (sym "graduationcap")
+    }
+    beat (tap, x 0) {
+        _on  (svg "icon.heartbeat.fill")
+        _off (svg "icon.heartbeat")
+    }
 }
